e70067de11f3843f4d26172096fb80c7532d42b6
Read Langworthy <rlangworthy@gmail.com>
2021-10-14 10:04:18 -0700

    commit tree 12cde2563a9169e451e01a6fcab2cdaa4ab05936

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const divide = function () {
+    return(2000 / 100);
+}
+
+
+console.log( divide() )
+
+const square = x => x*x;
+
+console.log( square(3) )
+
+
+const add = (x, y) => x+y;
+
+console.log( add(3, 4) )
+
